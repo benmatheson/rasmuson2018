61b0edd94df12ee905def3af6f88e72475c47f68
benmatheson <mathesonben@gmail.com>
2018-06-25 08:58:28 -0800

    scrollzoom arrow canvas

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@ var cities_fake = './data/city_labels_fake.geojson';
 
     var popup = new mapboxgl.Popup({
         closeButton: false,
-        closeOnClick: true
+        // closeOnClick: true
     });
 
 
